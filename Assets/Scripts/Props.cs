0b138f71e0ed7feaa222c41282af0c0c63b22ff1
Owen Greenaway <owengreenaway@hotmail.com>
2019-07-20 15:06:39 +0100

    Added launcher

---
+++ Assets/Scripts/Props.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Props : MonoBehaviour
+{
+    public GameObject rightHand;
+    public GameObject leftHand;
+    public Rigidbody ballPrefab;
+    public Vector3 ballOffset = new Vector3(0,0.3f,0);
+    public float ballSpeed = 2;
+
+    public void CreateBallAtRightHand()
+    {
+        CreateBall(rightHand);
+    }
+
+    public void CreateBallAtLeftHand()
+    {
+        CreateBall(leftHand);
+    }
+
+    private void CreateBall(GameObject hand)
+    {
+        var ballInstance = Instantiate(ballPrefab, hand.transform.position + ballOffset, hand.transform.rotation);
+        ballInstance.velocity = hand.transform.up * ballSpeed;
+    }
+}
